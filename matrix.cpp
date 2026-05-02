c901140c9f156cc7439082ea73e6b15e9b2026e1
Aidan-Z <aidanzamfir@gmail.com>
2026-05-02 12:35:50 +0300

    matrx class

--- matrix.cpp
+++ matrix.cpp
@@ -4,12 +4,37 @@
 //make class for matrix
 class Matrix{
 private:
+    int row;
+    int col;
 
 public:
+    //setter
+    void set_matrix(){
 
-    Matrix(){
-        //constrcutor
     }
+    //getter
+    void get_matrix(){
+
+    }
+    
+
+    Matrix(int rows, int column){
+        row = rows;
+        col = column;
+    }
+
+
+    //operations
+    /*
+
+    add
+    subtract
+    multiply
+
+    diagonalize
+    etc
+
+    */
 
 
 };
